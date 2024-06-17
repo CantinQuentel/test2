e5fac96c1328f78a93fdd6cf71f2683ff44b2920
CantinQuentel <cantinquentel9@gmail.com>
2024-06-17 13:21:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+AFRAME.registerComponent('scene-switch', {
+  schema: {
+    name : {type: 'string', default: 'default'}
+  },
+
+  init: async function() {
+    this.onClick = this.onClick.bind(this)
+
+    this.SceneName = this.data["name"]
+    this.el.addEventListener("click", this.onClick)
+  },
+
+  onClick: async function() {
+    // console.log("area")
+    SwitchArea(this.SceneName)
+  }
+})
 
